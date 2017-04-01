6a242972a0b901fdef0ba108507e941ed165f490
Nicole0320 <difan0320@126.com>
2017-04-01 15:10:49 +0800

    修改第一个webpack例子

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component(){
-	var element = document.createElement('div');
+	var element = $('<div></div>');
 
 	/* lodash is required for the next line to work*/
-	element.innerHTML = _.join(['hello','webpack'], ' ');
+	element.html(_.join(['Hello','webpack'], ' - '));
 
-	return element;
+	console.log(element);
+	return element.get(0);
 }
 
 document.body.appendChild(component());
