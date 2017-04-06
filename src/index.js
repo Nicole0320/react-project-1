fdd9838a5473241236d6ef3ec802b23248a196d5
Nicole0320 <difan0320@126.com>
2017-04-06 19:33:31 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <main>
+    <h1>Hello, world!</h1>
+    <h3>My very first React App</h3>
+  </main>,
+  document.getElementById('root2')
+);
