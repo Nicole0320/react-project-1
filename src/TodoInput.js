2546f6ade3860efe61edb18c3c50e3f0e7cde0e5
Nicole0320 <difan0320@126.com>
2017-04-12 17:33:03 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,9 +4,16 @@ class TodoInput extends React.Component{
     constructor(props){
         super(props);
     }
-    
+
     render(){
-        return <input type="text" defaultValue={this.props.content}/>;
+        return <input type="text" defaultValue={this.props.content}
+            onKeyPress={this.submit}/>;
+    }
+
+    submit(e){
+        if(e.key === 'Enter'){
+            console.log('用户按回车了！');
+        }
     }
 }
 
