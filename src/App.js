e8ff25d0cd8185c8cfe962c5ad75bdde640cfd42
Nicole0320 <difan0320@126.com>
2017-04-12 16:33:03 +0800

    添加normalize.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import TodoInput from './TodoInput'
-import TodoItem from './TodoItem'
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import 'normalize.css'
 
 class App extends Component {
   constructor(props){
