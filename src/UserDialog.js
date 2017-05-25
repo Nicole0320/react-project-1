939c53584337e98fb835dbbf5cd668a4790b237e
Nicole0320 <difan0320@126.com>
2017-05-25 22:05:22 +0800

    添加【忘记密码】按钮

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -120,6 +120,7 @@ export default class UserDialog extends Component{
                 </div>
                 <div className="row action">
                     <button type="submit">登录</button>
+                     <a href="javascript:;">忘记密码</a>
                 </div>
             </form>
         );
