fd540847165948ab3f39384f7bab97a99004bf50
Nicole0320 <difan0320@126.com>
2017-06-13 14:20:58 +0800

    TodoInput改成函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,41 +2,33 @@ import React from 'react';
 import './TodoInput.css';
 import './../public/iconfont/iconfont.css'
 
-class TodoInput extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return (
-        <form className="TodoInput" onSubmit={this.addItem.bind(this)}>
+export default function(props){
+    return (
+        <form className="TodoInput" onSubmit={addItem.bind(null, props)}>
             <input type="text" className="InputBar"
                 placeholder="添加待办事项..."
-                value={this.props.content}
-                onKeyPress={this.submit.bind(this)}
-            onChange={this.changeTitle.bind(this)}/>
+                value={props.content}
+                onKeyPress={submit.bind(null, props)}
+            onChange={changeTitle.bind(null, props)}/>
             <button type="submit"><i className="iconfont icon-tianjia"></i></button>
         </form>
-        );
-    }
+    );
+}
 
-    addItem(e){
-        e.preventDefault();
-        if(this.props.content !== ''){
-            this.props.onSubmit(this.props.content);
-        }
-    }
-
-    submit(e){
-        if(e.key === 'Enter' && e.target.value !== ''){
-        e.preventDefault();
-            this.props.onSubmit(e.target.value);
-        }
-    }
-
-    changeTitle(e){
-        this.props.onChange(e);
+function addItem(props, e){
+    e.preventDefault();
+    if(props.content !== ''){
+        props.onSubmit(props.content);
     }
 }
 
-export default TodoInput;
+function submit(props, e){
+    if(e.key === 'Enter' && e.target.value !== ''){
+    e.preventDefault();
+        props.onSubmit(e.target.value);
+    }
+}
+
+function changeTitle(props, e){
+    props.onChange(e);
+}
